b0d709a52de386914859d1410413549469f71aea
ranaelbaga <162725775+ranaelbaga@users.noreply.github.com>
2024-03-16 16:48:13 +0200

    .

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'lab3database',
-        'PASSWORD': 'Mypassword3',
-        'HOST': 'ehrserver.postgres.database.azure.com',
+        'USER': 'rana',
+        'PASSWORD': 'Rana1411',
+        'HOST': 'rana1210134.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS':{'sslmode':'require'},
     }
